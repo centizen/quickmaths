53c298c54630fbea4e83647d1b27ccd2a41b6628
centizen <centizen@centizen.ca>
2023-03-07 20:24:54 -0500

    Cleanup file structure

--- QuickMaths/Plugin.cs
+++ QuickMaths/Plugin.cs
@@ -5,7 +5,7 @@ using Dalamud.Game.Gui;
 using System.Data;
 using System;
 
-namespace QuickMaths
+namespace QuickMaths.QuickMaths
 {
     public sealed class Plugin : IDalamudPlugin
     {
@@ -15,26 +15,26 @@ namespace QuickMaths
         private DalamudPluginInterface PluginInterface { get; init; }
         private CommandManager CommandManager { get; init; }
         private ChatGui ChatGui { get; init; }
- 
- 
+
+
         public Plugin(
             [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
             [RequiredVersion("1.0")] CommandManager commandManager,
             [RequiredVersion("1.0")] ChatGui chatGui)
         {
-            this.PluginInterface = pluginInterface;
-            this.CommandManager = commandManager;
-            this.ChatGui = chatGui;
+            PluginInterface = pluginInterface;
+            CommandManager = commandManager;
+            ChatGui = chatGui;
 
-            this.CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+            CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
             {
-                HelpMessage = "Quick and dirty chatlog calculator. No need for any extra windows! \n\nSigns \n +  =  Addition \n -  =  Subtraction \n *  =  Multiplication \n /  =  Division \n\nExamples: \n/pcalc 2 + 2 \n/pcalc 1 + 2 * 3 / 4 \n/pcalc 10 * (5 * 2) - 2\n\n\n" 
+                HelpMessage = "Quick and dirty chatlog calculator. No need for any extra windows! \n\nSigns \n +  =  Addition \n -  =  Subtraction \n *  =  Multiplication \n /  =  Division \n\nExamples: \n/pcalc 2 + 2 \n/pcalc 1 + 2 * 3 / 4 \n/pcalc 10 * (5 * 2) - 2\n\n\n"
             });
         }
 
         public void Dispose()
         {
-            this.CommandManager.RemoveHandler(CommandName);
+            CommandManager.RemoveHandler(CommandName);
         }
 
         private void OnCommand(string command, string args)
@@ -42,13 +42,13 @@ namespace QuickMaths
             try
             {
                 var result = new DataTable().Compute(args, null);
-                this.ChatGui.Print("" + args + " = " + result);
-                this.ChatGui.UpdateQueue();
+                ChatGui.Print("" + args + " = " + result);
+                ChatGui.UpdateQueue();
             }
             catch (Exception)
             {
-                this.ChatGui.PrintError("QuickMaths: Error in expression");
-                this.ChatGui.UpdateQueue();
+                ChatGui.PrintError("QuickMaths: Error in expression");
+                ChatGui.UpdateQueue();
             }
         }
     }
